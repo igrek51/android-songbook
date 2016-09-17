0450e2ca0180cde1c497203f57fc46dc29ec75c8
igrek51 <igrek.s@o2.pl>
2016-09-17 13:18:54 +0200

    Changed build.gradle - saving version name, Added release gen script, Added force clean script

---
+++ force-clean.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# wykonać w przypadku błędu cleana przez Android Studio, które samo blokuje sobie pliki do usunięcia, KURWA!!!
+
+import sys
+from subprocess import call, check_output
+
+def shellExec(cmd):
+	errCode = call(cmd, shell=True)
+	if errCode != 0:
+		fatalError('failed executing: ' + cmd)
+
+def fatalError(message):
+	print '[ERROR] ' + message
+	sys.exit()
+
+shellExec('rm -rf app/build')
+
+print 'done'
---
+++ release/make-release.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import subprocess
+import os
+
+apkSrc = '../app/build/outputs/apk/app-debug.apk'
+# TODO wyciągnąć numer wersji z app/version.properties
+versionName = '1.0.2'
+
+apkOutputFile = 'SongBook-' + versionName + '-debug.apk'
+outputDBArchive = 'SongBook-db-' + versionName + '.zip'
+releaseLinkName = 'SongBook-apkdb-release.zip'
+
+def removeIfExists(fileName):
+	if os.path.exists(fileName):
+		print 'removing ' + fileName + '...'
+		subprocess.call('rm '+fileName, shell=True)
+
+removeIfExists(outputDBArchive)
+
+removeIfExists(apkOutputFile)
+subprocess.call('cp ' + apkSrc + ' ' + apkOutputFile, shell=True)
+
+subprocess.call('zip -r '+outputDBArchive+' GuitarDB -x *.git*', shell=True)
+
+# link do aktualnej wersji
+# removeIfExists(releaseLinkName)
+# subprocess.call('ln -s ' + outputArchive + ' ' + releaseLinkName, shell=True)
+
+print 'done'
