8e5eee32e66404e37ef896d22b69be41c71ceb11
igrek51 <igrek.s@o2.pl>
2018-09-03 23:55:32 +0200

    Porting Navigation menu to Kotlin

---
+++ app/src/main/java/igrek/songbook/service/layout/navmenu/NavigationMenuController.kt
@@ -0,0 +1,75 @@
+package igrek.songbook.service.layout.navmenu
+
+import android.app.Activity
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import igrek.songbook.R
+import igrek.songbook.dagger.DaggerIoc
+import igrek.songbook.logger.LoggerFactory
+import igrek.songbook.service.activity.ActivityController
+import igrek.songbook.service.errorcheck.SafeExecutor
+import igrek.songbook.service.info.UiInfoService
+import igrek.songbook.service.layout.songselection.SongSelectionController
+import java.util.*
+import javax.inject.Inject
+
+class NavigationMenuController {
+
+    private var drawerLayout: DrawerLayout? = null
+    private var navigationView: NavigationView? = null
+    private val actionsMap = HashMap<Int, Runnable>()
+    private val logger = LoggerFactory.getLogger()
+
+    @Inject
+    lateinit var activity: Activity
+    @Inject
+    lateinit var uiInfoService: UiInfoService
+    @Inject
+    lateinit var songSelectionController: SongSelectionController
+    @Inject
+    lateinit var activityController: ActivityController
+
+    init {
+        DaggerIoc.getFactoryComponent().inject(this)
+        initOptionActionsMap()
+    }
+
+    private fun initOptionActionsMap() {
+        actionsMap[R.id.nav_songs_list] = Runnable { uiInfoService.showToast("not implemented yet") }
+        actionsMap[R.id.nav_search] = Runnable { uiInfoService.showToast("not implemented yet") }
+        actionsMap[R.id.nav_update_db] = Runnable { uiInfoService.showToast("not implemented yet") }
+        actionsMap[R.id.nav_import_song] = Runnable { uiInfoService.showToast("not implemented yet") }
+        actionsMap[R.id.nav_settings] = Runnable { uiInfoService.showToast("not implemented yet") }
+        actionsMap[R.id.nav_help] = Runnable { songSelectionController.showUIHelp() }
+        actionsMap[R.id.nav_about] = Runnable { songSelectionController.showAbout() }
+        actionsMap[R.id.nav_exit] = Runnable { activityController.quit() }
+    }
+
+    fun init() {
+        drawerLayout = activity.findViewById(R.id.drawer_layout)
+        navigationView = activity.findViewById(R.id.nav_view)
+
+        navigationView!!.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            drawerLayout!!.closeDrawers()
+            val id = menuItem.itemId
+            if (actionsMap.containsKey(id)) {
+                val action = actionsMap[id]
+                SafeExecutor().execute(action)
+            } else {
+                logger.warn("unknown navigation item has been selected.")
+            }
+            true
+        }
+    }
+
+    fun navDrawerShow() {
+        drawerLayout!!.openDrawer(GravityCompat.START)
+        // unhighlight all menu items
+        for (id in 0 until navigationView!!.menu.size())
+            navigationView!!.menu.getItem(id).isChecked = false
+    }
+
+}
