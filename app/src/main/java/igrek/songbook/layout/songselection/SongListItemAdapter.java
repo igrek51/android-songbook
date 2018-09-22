b0c6c281da5aa6d627c03d5f695d906d982b38db
igrek51 <igrek.s@o2.pl>
2018-09-22 14:42:14 +0200

    Add Song tree category and note icons

--- app/src/main/java/igrek/songbook/layout/songselection/SongListItemAdapter.java
+++ app/src/main/java/igrek/songbook/layout/songselection/SongListItemAdapter.java
@@ -1,7 +1,6 @@
 package igrek.songbook.layout.songselection;
 
 import android.content.Context;
-import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -52,33 +51,50 @@ public class SongListItemAdapter extends ArrayAdapter<SongTreeItem> {
 	
 	@Override
 	public View getView(final int position, View convertView, ViewGroup parent) {
-		
+		SongTreeItem item = dataSource.get(position);
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		
-		View itemView = inflater.inflate(R.layout.song_list_item, parent, false);
-		SongTreeItem item = dataSource.get(position);
-		
-		// set item title
-		TextView itemContentLabel = itemView.findViewById(R.id.itemContentLabel);
-		if (item.isCategory()) {
-			itemContentLabel.setTypeface(null, Typeface.BOLD);
-		} else {
-			itemContentLabel.setTypeface(null, Typeface.NORMAL);
-		}
 		if (item instanceof SongSearchItem) {
-			// search item: title - category
-			String songTitle = item.getSong().getTitle();
-			String categoryName = item.getSong().getCategory().getName();
-			String displayName;
-			if (categoryName != null) {
-				displayName = songTitle + " - " + categoryName;
-			} else {
-				displayName = songTitle;
-			}
-			itemContentLabel.setText(displayName);
-		} else {
-			itemContentLabel.setText(item.getSimpleName());
+			return createSearchSongView((SongSearchItem) item, inflater, parent);
 		}
+		if (item.isCategory()) {
+			return createTreeCategoryView(item, inflater, parent);
+		} else {
+			return createTreeSongView(item, inflater, parent);
+		}
+	}
+	
+	private View createTreeCategoryView(SongTreeItem item, LayoutInflater inflater, ViewGroup parent) {
+		View itemView = inflater.inflate(R.layout.song_tree_category, parent, false);
+		TextView itemCategoryNameLabel = itemView.findViewById(R.id.itemCategoryNameLabel);
+		// set item title
+		itemCategoryNameLabel.setText(item.getSimpleName());
+		
+		return itemView;
+	}
+	
+	private View createTreeSongView(SongTreeItem item, LayoutInflater inflater, ViewGroup parent) {
+		View itemView = inflater.inflate(R.layout.song_tree_song, parent, false);
+		TextView itemSongTitleLabel = itemView.findViewById(R.id.itemSongTitleLabel);
+		// set item title
+		itemSongTitleLabel.setText(item.getSimpleName());
+		
+		return itemView;
+	}
+	
+	private View createSearchSongView(SongSearchItem item, LayoutInflater inflater, ViewGroup parent) {
+		View itemView = inflater.inflate(R.layout.song_search_song, parent, false);
+		TextView itemSongLabel = itemView.findViewById(R.id.itemSongLabel);
+		// search item: title - category
+		String songTitle = item.getSong().getTitle();
+		String categoryName = item.getSong().getCategory().getName();
+		String displayName;
+		if (categoryName != null) {
+			displayName = songTitle + " - " + categoryName;
+		} else {
+			displayName = songTitle;
+		}
+		itemSongLabel.setText(displayName);
 		
 		return itemView;
 	}
