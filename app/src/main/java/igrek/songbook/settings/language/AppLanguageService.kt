49436cbdc3d19f93dc0678eba5101120c816a6c7
igrek51 <igrek.s@o2.pl>
2018-11-20 02:48:55 +0100

    Setting default chords notation depending on locale settings

--- app/src/main/java/igrek/songbook/settings/language/AppLanguageService.kt
+++ app/src/main/java/igrek/songbook/settings/language/AppLanguageService.kt
@@ -1,11 +1,15 @@
 package igrek.songbook.settings.language
 
 import android.app.Activity
+import android.os.Build
 import igrek.songbook.dagger.DaggerIoc
+import igrek.songbook.model.chords.ChordsNotation
 import igrek.songbook.persistence.preferences.PreferencesDefinition
 import igrek.songbook.persistence.preferences.PreferencesService
+import igrek.songbook.songpreview.LyricsManager
 import java.util.*
 import javax.inject.Inject
+
 
 class AppLanguageService {
 
@@ -13,12 +17,29 @@ class AppLanguageService {
     lateinit var activity: Activity
     @Inject
     lateinit var preferencesService: PreferencesService
+    @Inject
+    lateinit var lyricsManager: LyricsManager
 
     var appLanguage: AppLanguage? = null
 
     init {
         DaggerIoc.getFactoryComponent().inject(this)
         loadPreferences()
+        setDefaultChordsNotation()
+    }
+
+    private fun setDefaultChordsNotation() {
+        // running for the first time - set german / polish notation if lang pl
+        // set default chords notation depending on locale settings
+        if (!preferencesService.exists(PreferencesDefinition.chordsNotationId.name)) {
+            val current: Locale = getCurrentLocale()
+            val lang = current.language
+            if (lang == "pl") {
+                lyricsManager.chordsNotation = ChordsNotation.GERMAN
+            } else {
+                lyricsManager.chordsNotation = ChordsNotation.ENGLISH
+            }
+        }
     }
 
     private fun loadPreferences() {
@@ -42,6 +63,14 @@ class AppLanguageService {
     fun setLocale() {
         if (appLanguage != null && appLanguage != AppLanguage.SYSTEM_DEFAULT) {
             setLocale(appLanguage!!.langCode)
+        }
+    }
+
+    fun getCurrentLocale(): Locale {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            activity.resources.configuration.locales.get(0)
+        } else {
+            activity.resources.configuration.locale
         }
     }
 }
