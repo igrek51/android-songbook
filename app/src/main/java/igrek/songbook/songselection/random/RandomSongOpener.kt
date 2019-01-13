071e451422c242e60c4659c542c4081255cbdba7
igrek51 <igrek.s@o2.pl>
2019-01-13 22:03:35 +0100

    Opening random songs from favourites

--- app/src/main/java/igrek/songbook/songselection/random/RandomSongOpener.kt
+++ app/src/main/java/igrek/songbook/songselection/random/RandomSongOpener.kt
@@ -1,12 +1,15 @@
 package igrek.songbook.songselection.random
 
+import igrek.songbook.R
 import igrek.songbook.dagger.DaggerIoc
+import igrek.songbook.info.UiInfoService
 import igrek.songbook.layout.LayoutController
 import igrek.songbook.model.songsdb.Song
 import igrek.songbook.persistence.SongsRepository
 import igrek.songbook.settings.preferences.PreferencesDefinition
 import igrek.songbook.settings.preferences.PreferencesService
 import igrek.songbook.songpreview.SongPreviewLayoutController
+import igrek.songbook.songselection.favourite.FavouriteSongsRepository
 import java.util.*
 import javax.inject.Inject
 
@@ -20,6 +23,10 @@ class RandomSongOpener {
     lateinit var songPreviewLayoutController: dagger.Lazy<SongPreviewLayoutController>
     @Inject
     lateinit var preferencesService: PreferencesService
+    @Inject
+    lateinit var favouriteSongsRepository: FavouriteSongsRepository
+    @Inject
+    lateinit var uiInfoService: UiInfoService
 
     var fromFavouriteSongsOnly: Boolean = false
 
@@ -34,15 +41,27 @@ class RandomSongOpener {
 
     fun openRandomSong() {
         val randomSong = getRandomSong()
-        if (randomSong != null)
-            openSongPreview(randomSong)
+        if (randomSong == null) {
+            uiInfoService.showInfo(R.string.no_songs_to_shuffle)
+            return
+        }
+
+        openSongPreview(randomSong)
     }
 
     private fun getRandomSong(): Song? {
-        val allSongs = songsRepository.songsDb!!.getAllUnlockedSongs()
-        if (allSongs.isEmpty())
+        val songsCollection = songsToShuffle()
+        if (songsCollection.isEmpty())
             return null
-        return allSongs[Random().nextInt(allSongs.size)]
+        return songsCollection[Random().nextInt(songsCollection.size)]
+    }
+
+    private fun songsToShuffle(): List<Song> {
+        return if (fromFavouriteSongsOnly) {
+            favouriteSongsRepository.getFavouriteSongs().toList()
+        } else {
+            songsRepository.songsDb!!.getAllUnlockedSongs()
+        }
     }
 
     private fun openSongPreview(song: Song) {
