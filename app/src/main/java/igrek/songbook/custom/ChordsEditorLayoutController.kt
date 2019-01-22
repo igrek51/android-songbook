fe86b420b6031a7e447b0407f378e1ddb94b1664
igrek51 <igrek.s@o2.pl>
2019-01-22 01:05:07 +0100

    Copying and pasting chords

--- app/src/main/java/igrek/songbook/custom/ChordsEditorLayoutController.kt
+++ app/src/main/java/igrek/songbook/custom/ChordsEditorLayoutController.kt
@@ -10,6 +10,7 @@ import dagger.Lazy
 import igrek.songbook.R
 import igrek.songbook.dagger.DaggerIoc
 import igrek.songbook.info.UiInfoService
+import igrek.songbook.info.UiResourceService
 import igrek.songbook.info.errorcheck.SafeClickListener
 import igrek.songbook.layout.LayoutController
 import igrek.songbook.layout.LayoutState
@@ -25,6 +26,8 @@ class ChordsEditorLayoutController : MainLayout {
     @Inject
     lateinit var uiInfoService: UiInfoService
     @Inject
+    lateinit var uiResourceService: UiResourceService
+    @Inject
     lateinit var activity: AppCompatActivity
     @Inject
     lateinit var navigationMenuController: NavigationMenuController
@@ -36,6 +39,7 @@ class ChordsEditorLayoutController : MainLayout {
     lateinit var softKeyboardService: SoftKeyboardService
 
     private var contentEdit: EditText? = null
+    private var clipboardChords: String? = null
 
     init {
         DaggerIoc.getFactoryComponent().inject(this)
@@ -70,9 +74,62 @@ class ChordsEditorLayoutController : MainLayout {
             }
         })
 
+        val copyChordButton = layout.findViewById<Button>(R.id.copyChordButton)
+        copyChordButton.setOnClickListener(object : SafeClickListener() {
+            override fun onClick() {
+                onCopyChordClick()
+            }
+        })
+
+        val pasteChordButton = layout.findViewById<Button>(R.id.pasteChordButton)
+        pasteChordButton.setOnClickListener(object : SafeClickListener() {
+            override fun onClick() {
+                onPasteChordClick()
+            }
+        })
+
         softKeyboardService.showSoftKeyboard(contentEdit)
 
-        // TODO buttons: copying, pasting, mutliple clipbords (for chords), auto chords finding, changing notation
+        // TODO buttons: mutliple clipbords (for chords), auto chords finding, changing notation
+    }
+
+    private fun onPasteChordClick() {
+        if (clipboardChords.isNullOrEmpty()) {
+            uiInfoService.showToast(R.string.paste_chord_empty)
+            return
+        }
+
+        var edited = contentEdit!!.text.toString()
+        val selStart = contentEdit!!.selectionStart
+        var selEnd = contentEdit!!.selectionEnd
+        val before = edited.substring(0, selStart)
+        val after = edited.substring(selEnd)
+
+        edited = "$before[$clipboardChords]$after"
+        selEnd += 2 + clipboardChords!!.length
+
+        contentEdit!!.setText(edited)
+        contentEdit!!.setSelection(selStart, selEnd)
+        contentEdit!!.requestFocus()
+    }
+
+    private fun onCopyChordClick() {
+        val edited = contentEdit!!.text.toString()
+        val selStart = contentEdit!!.selectionStart
+        val selEnd = contentEdit!!.selectionEnd
+
+        var selection = edited.substring(selStart, selEnd)
+        if (selection.startsWith("["))
+            selection = selection.drop(1)
+        if (selection.endsWith("]"))
+            selection = selection.dropLast(1)
+        clipboardChords = selection.trim()
+
+        if (clipboardChords.isNullOrEmpty()) {
+            uiInfoService.showToast(R.string.copy_chord_empty)
+        } else {
+            uiInfoService.showToast(uiResourceService.resString(R.string.chords_copied, clipboardChords))
+        }
     }
 
     override fun getLayoutState(): LayoutState {
@@ -107,14 +164,11 @@ class ChordsEditorLayoutController : MainLayout {
 
         // if there's nonempty selection
         if (selStart < selEnd) {
-
             val selected = edited.substring(selStart, selEnd)
             edited = "$before[$selected]$after"
             selStart++
             selEnd++
-
         } else { // just single cursor
-
             // if it's the end of line AND there is no space before
             if ((after.isEmpty() || after.startsWith("\n")) && !before.isEmpty() && !before.endsWith(" ")) {
                 // insert missing space
@@ -125,7 +179,6 @@ class ChordsEditorLayoutController : MainLayout {
                 selStart += 1
             }
             selEnd = selStart
-
         }
 
         contentEdit!!.setText(edited)
