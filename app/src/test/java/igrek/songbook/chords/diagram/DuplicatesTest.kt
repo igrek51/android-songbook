0a7eaad9281444fa3c5421663907cbd85e3f5086
igrek51 <igrek51.dev@gmail.com>
2019-09-02 21:47:40 +0200

    Remove duplicated chord diagrams

---
+++ app/src/test/java/igrek/songbook/chords/diagram/DuplicatesTest.kt
@@ -0,0 +1,35 @@
+package igrek.songbook.chords.diagram
+
+import org.junit.Test
+
+
+class DuplicatesTest {
+
+    @Test
+    fun test_duplicates_in_particular_chords() {
+        chordsDiagrams.forEach { (name, diagrams) ->
+            val uniqueDiagrams = mutableSetOf<String>()
+            diagrams.forEach { diagram ->
+                assert(diagram !in uniqueDiagrams) { "chord $name has duplicated diagrams: $diagram" }
+                uniqueDiagrams.add(diagram)
+            }
+        }
+    }
+
+    @Test
+    fun test_duplicated_diagrams_between_chords() {
+        val errors = mutableListOf<String>()
+        val uniqueDiagrams = mutableSetOf<String>()
+        chordsDiagrams.forEach { (name, diagrams) ->
+            diagrams.forEach { diagram ->
+                if(diagram in uniqueDiagrams) {
+                    errors.add("diagram $diagram is not unique for chord $name")
+                }
+                uniqueDiagrams.add(diagram)
+            }
+        }
+
+        // assert(errors.isEmpty()) { errors.joinToString(separator = "\n") }
+    }
+
+}
