2981a68e1f4bde2500e9277d2ac384bdf1658a4d
igrek51 <igrek51.dev@gmail.com>
2020-05-19 20:56:37 +0200

    Find diagrams for given chord

--- app/src/main/java/igrek/songbook/chords/diagram/ChordsDiagramsService.kt
+++ app/src/main/java/igrek/songbook/chords/diagram/ChordsDiagramsService.kt
@@ -3,7 +3,11 @@ package igrek.songbook.chords.diagram
 
 import android.app.Activity
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.text.InputType
 import android.view.LayoutInflater
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import igrek.songbook.R
@@ -21,6 +25,7 @@ import igrek.songbook.settings.chordsnotation.ChordsNotationService
 import igrek.songbook.settings.instrument.ChordsInstrument
 import igrek.songbook.settings.instrument.ChordsInstrumentService
 import igrek.songbook.settings.preferences.PreferencesState
+import igrek.songbook.system.SoftKeyboardService
 import java.util.*
 
 class ChordsDiagramsService(
@@ -31,6 +36,7 @@ class ChordsDiagramsService(
         chordsInstrumentService: LazyInject<ChordsInstrumentService> = appFactory.chordsInstrumentService,
         chordsNotationService: LazyInject<ChordsNotationService> = appFactory.chordsNotationService,
         preferencesState: LazyInject<PreferencesState> = appFactory.preferencesState,
+        softKeyboardService: LazyInject<SoftKeyboardService> = appFactory.softKeyboardService,
 ) {
     private val uiInfoService by LazyExtractor(uiInfoService)
     private val uiResourceService by LazyExtractor(uiResourceService)
@@ -39,6 +45,7 @@ class ChordsDiagramsService(
     private val chordsInstrumentService by LazyExtractor(chordsInstrumentService)
     private val chordsNotationService by LazyExtractor(chordsNotationService)
     private val preferencesState by LazyExtractor(preferencesState)
+    private val softKeyboardService by LazyExtractor(softKeyboardService)
 
     private var toEnglishConverter = ChordsConverter(ChordsNotation.default, ChordsNotation.ENGLISH)
 
@@ -101,8 +108,13 @@ class ChordsDiagramsService(
         alertBuilder.setCancelable(true)
 
         alertBuilder.setPositiveButton(uiResourceService.resString(R.string.action_close)) { _, _ -> }
-        alertBuilder.setNeutralButton(uiResourceService.resString(R.string.action_back)) { _, _ ->
-            showUniqueChordsMenu(uniqueChords)
+        if (uniqueChords.isNotEmpty()) {
+            alertBuilder.setNeutralButton(uiResourceService.resString(R.string.action_back)) { _, _ ->
+                showUniqueChordsMenu(uniqueChords)
+            }
+        }
+        alertBuilder.setNegativeButton(uiResourceService.resString(R.string.action_find_chord)) { _, _ ->
+            showFindChordsMenu()
         }
 
         val inflater = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -112,6 +124,37 @@ class ChordsDiagramsService(
         alertBuilder.setView(diagramView)
 
         alertBuilder.create().show()
+    }
+
+    private fun showFindChordsMenu() {
+        val dlgAlert = AlertDialog.Builder(activity)
+        dlgAlert.setTitle(uiResourceService.resString(R.string.chord_diagram_find_chord))
+
+        val input = EditText(activity)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        dlgAlert.setView(input)
+
+        dlgAlert.setPositiveButton(uiResourceService.resString(R.string.action_find_chord)) { _, _ ->
+            tryToFindChordDiagram(input.text.toString())
+        }
+        dlgAlert.setNegativeButton(uiResourceService.resString(R.string.action_cancel)) { _, _ -> }
+        dlgAlert.setCancelable(true)
+        dlgAlert.create().show()
+
+        input.requestFocus()
+        Handler(Looper.getMainLooper()).post {
+            softKeyboardService.showSoftKeyboard(input)
+        }
+    }
+
+    private fun tryToFindChordDiagram(chordName: String) {
+        val chordName = chordName.trim()
+        val (_, errors) = toEnglishConverter.convertChordsGroup(chordName)
+        if (errors.isNotEmpty()) {
+            uiInfoService.showInfo(uiResourceService.resString(R.string.chord_diagram_not_found))
+            return
+        }
+        showChordDefinition(chordName, emptySet())
     }
 
     fun chordDiagramStyleEntries(): LinkedHashMap<String, String> {
