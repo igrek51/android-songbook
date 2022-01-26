b092bb6f97d05b55e3981fb41b50199953b9a8c6
igrek51 <igrek51.dev@gmail.com>
2022-01-26 16:09:13 +0100

    Remove redundant privileges

---
+++ app/src/androidTest/java/igrek/songbook/test/ScreenshotCapture.kt
@@ -0,0 +1,40 @@
+package igrek.songbook.test
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.screenshot.ScreenCapture
+import androidx.test.runner.screenshot.Screenshot
+import java.io.File
+
+class ScreenshotCapture {
+
+    companion object {
+        fun takeScreenshot(name: String) {
+            val capture: ScreenCapture = Screenshot.capture()
+            val bmp: Bitmap = capture.bitmap
+            val trimTop = 0
+            val trimBottom = 132
+            val cropped: Bitmap = Bitmap.createBitmap(bmp, 0, trimTop, bmp.width, bmp.height - trimTop - trimBottom)
+
+            val testContext: Context = InstrumentationRegistry.getInstrumentation().context
+            var packageName = testContext.packageName
+            if (packageName.endsWith(".test"))
+                packageName = packageName.removeSuffix(".test")
+
+            val testDataDir = File("/storage/emulated/0/Android/data/${packageName}/files/screenshots")
+            testDataDir.mkdirs()
+            val screenshotFile = File(testDataDir, "${name}.png")
+
+            writeBitmap(screenshotFile, cropped, Bitmap.CompressFormat.PNG, 90)
+        }
+
+        private fun writeBitmap(file: File, bitmap: Bitmap, format: Bitmap.CompressFormat, quality: Int) {
+            file.outputStream().use { out ->
+                bitmap.compress(format, quality, out)
+                out.flush()
+            }
+        }
+    }
+
+}
