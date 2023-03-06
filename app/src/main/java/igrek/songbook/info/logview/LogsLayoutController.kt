3169045016029478b50a2a9d728fc7911adc377b
igrek51 <igrek51.dev@gmail.com>
2023-03-06 21:37:12 +0100

    Copy logs on clicked item

--- app/src/main/java/igrek/songbook/info/logview/LogsLayoutController.kt
+++ app/src/main/java/igrek/songbook/info/logview/LogsLayoutController.kt
@@ -4,18 +4,22 @@ import android.annotation.SuppressLint
 import android.view.View
 import igrek.songbook.R
 import igrek.songbook.info.UiInfoService
+import igrek.songbook.info.logger.LogEntry
 import igrek.songbook.info.logger.LoggerFactory
 import igrek.songbook.inject.LazyExtractor
 import igrek.songbook.inject.LazyInject
 import igrek.songbook.inject.appFactory
 import igrek.songbook.layout.InflatedLayout
+import igrek.songbook.system.ClipboardManager
 
 @SuppressLint("CheckResult")
 class LogsLayoutController(
+    clipboardManager: LazyInject<ClipboardManager> = appFactory.clipboardManager,
     uiInfoService: LazyInject<UiInfoService> = appFactory.uiInfoService,
 ) : InflatedLayout(
     _layoutResourceId = R.layout.screen_logs
 ) {
+    private val clipboardManager by LazyExtractor(clipboardManager)
     private val uiInfoService by LazyExtractor(uiInfoService)
 
     private var itemsListView: LogListView? = null
@@ -24,7 +28,9 @@ class LogsLayoutController(
         super.showLayout(layout)
 
         itemsListView = layout.findViewById<LogListView>(R.id.itemsListView)?.also {
-            it.onClickCallback = {}
+            it.onClickCallback = { item: LogEntry ->
+                copyItemToClipboard(item)
+            }
             it.items = listOf()
             it.emptyView = layout.findViewById(R.id.emptyListTextView)
         }
@@ -37,5 +43,10 @@ class LogsLayoutController(
 
     private fun populateItems(listView: LogListView) {
         listView.items = LoggerFactory.sessionLogs.reversed()
+    }
+
+    private fun copyItemToClipboard(item: LogEntry) {
+        clipboardManager.copyToSystemClipboard(item.message)
+        uiInfoService.showInfo(R.string.copied_to_clipboard)
     }
 }
