a4ab121a45ca00ef041fe93b9c9b3291ad629b99
igrek51 <igrek51.dev@gmail.com>
2023-05-04 21:37:12 +0200

    Turn song ID into string

--- app/src/main/java/igrek/songbook/persistence/general/mapper/SongCategoryMapper.kt
+++ app/src/main/java/igrek/songbook/persistence/general/mapper/SongCategoryMapper.kt
@@ -8,6 +8,6 @@ class SongCategoryMapper : AbstractMapper<SongCategoryRelationship>() {
     override fun map(cursor: Cursor): SongCategoryRelationship {
         val songId = cursor.getLong(cursor.getColumnIndexOrThrow("song_id"))
         val categoryId = cursor.getLong(cursor.getColumnIndexOrThrow("category_id"))
-        return SongCategoryRelationship(songId, categoryId)
+        return SongCategoryRelationship(songId.toString(), categoryId)
     }
 }
--- app/src/main/java/igrek/songbook/persistence/general/model/SongCategoryRelationship.kt
+++ app/src/main/java/igrek/songbook/persistence/general/model/SongCategoryRelationship.kt
@@ -1,6 +1,6 @@
 package igrek.songbook.persistence.general.model
 
 data class SongCategoryRelationship(
-    var song_id: Long,
-    var category_id: Long
+    var song_id: String,
+    var category_id: Long,
 )
