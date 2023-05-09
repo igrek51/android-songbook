1b89c07573bfa8a740b4454c9bfaeda8bf3e75f5
igrek51 <igrek51.dev@gmail.com>
2023-05-09 15:11:50 +0200

    Use Jetpack Compose in Song Cast Layout

--- app/src/main/java/igrek/songbook/cast/SongCastLayout.kt
+++ app/src/main/java/igrek/songbook/cast/SongCastLayout.kt
@@ -1,20 +1,53 @@
+@file:OptIn(DelicateCoroutinesApi::class)
+
 package igrek.songbook.cast
 
 import android.view.View
-import android.widget.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import igrek.songbook.R
+import igrek.songbook.admin.ApiResponseError
+import igrek.songbook.compose.AppTheme
+import igrek.songbook.compose.LabelText
+import igrek.songbook.compose.RichText
+import igrek.songbook.compose.md_theme_light_primaryContainer
 import igrek.songbook.info.UiInfoService
+import igrek.songbook.info.errorcheck.LocalizedError
+import igrek.songbook.info.errorcheck.UiErrorHandler
+import igrek.songbook.info.errorcheck.safeExecute
 import igrek.songbook.inject.LazyExtractor
 import igrek.songbook.inject.LazyInject
 import igrek.songbook.inject.appFactory
 import igrek.songbook.layout.InflatedLayout
 import kotlinx.coroutines.*
-import com.google.android.material.textfield.TextInputLayout
-import igrek.songbook.admin.ApiResponseError
-import igrek.songbook.info.errorcheck.LocalizedError
-import igrek.songbook.info.errorcheck.SafeClickListener
-import igrek.songbook.info.errorcheck.UiErrorHandler
-import igrek.songbook.info.errorcheck.safeExecute
+
 
 @OptIn(DelicateCoroutinesApi::class)
 class SongCastLayout(
@@ -26,9 +59,7 @@ class SongCastLayout(
     private val uiInfoService by LazyExtractor(uiInfoService)
     private val songCastService by LazyExtractor(songCastService)
 
-    private var nameInput: TextInputLayout? = null
-    private var roomCodeInput: TextInputLayout? = null
-    private var myName: String = ""
+    val stateModel = StateModel()
 
     override fun showLayout(layout: View) {
         super.showLayout(layout)
@@ -40,49 +71,33 @@ class SongCastLayout(
             return
         }
 
-        nameInput = layout.findViewById<TextInputLayout?>(R.id.nameInput)?.also {
-            it.editText?.setText(myName)
-
-            it.setEndIconOnClickListener {
-                randomizeName()
-            }
-        }
-        if (myName.isBlank()) {
+        if (stateModel.myName.isBlank()) {
             randomizeName()
         }
 
-        roomCodeInput = layout.findViewById(R.id.roomCodeInput)
-
-        layout.findViewById<Button>(R.id.createNewRoomButton)
-            ?.setOnClickListener {
-                GlobalScope.launch {
-                    safeExecute {
-                        createRoom()
-                    }
+        val thisLayout = this
+        layout.findViewById<ComposeView>(R.id.compose_view).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                AppTheme {
+                    MainPage(thisLayout)
                 }
             }
-
-        layout.findViewById<Button>(R.id.joinRoomButton)
-            ?.setOnClickListener(SafeClickListener {
-                GlobalScope.launch {
-                    safeExecute {
-                        joinRoom()
-                    }
-                }
-            })
+        }
     }
 
-    private fun randomizeName() {
-        nameInput?.editText?.setText(AnimalNameFeeder().generateName())
+    fun randomizeName() {
+        stateModel.myName = AnimalNameFeeder().generateName()
     }
 
     private fun getMemberName(): String {
-        myName = nameInput?.editText?.text.toString().takeIf { it.isNotBlank() }
-            ?: AnimalNameFeeder().generateName()
-        return myName
+        if (stateModel.myName.isBlank()) {
+            stateModel.myName = AnimalNameFeeder().generateName()
+        }
+        return stateModel.myName
     }
 
-    private suspend fun createRoom() {
+    suspend fun createRoom() {
         uiInfoService.showInfo(R.string.songcast_creating_room, indefinite = true)
         val result = songCastService.createSessionAsync(getMemberName()).await()
         result.fold(onSuccess = { response: CastSessionJoined ->
@@ -90,8 +105,15 @@ class SongCastLayout(
                 GlobalScope.launch(Dispatchers.Main) {
                     delay(100) // WTF: Android hackaround
                     when (response.rejoined) {
-                        true -> uiInfoService.showInfo(R.string.songcast_room_created_rejoined, response.member_name)
-                        false -> uiInfoService.showInfo(R.string.songcast_room_created, response.member_name)
+                        true -> uiInfoService.showInfo(
+                            R.string.songcast_room_created_rejoined,
+                            response.member_name
+                        )
+
+                        false -> uiInfoService.showInfo(
+                            R.string.songcast_room_created,
+                            response.member_name
+                        )
                     }
                 }
             }
@@ -100,8 +122,8 @@ class SongCastLayout(
         })
     }
 
-    private suspend fun joinRoom() {
-        val roomCode = roomCodeInput?.editText?.text.toString().replace(" ", "").trim()
+    suspend fun joinRoom() {
+        val roomCode = stateModel.roomCode.replace(" ", "").trim()
         if (roomCode.isBlank()) {
             throw LocalizedError(R.string.songcast_room_code_empty)
         }
@@ -116,8 +138,15 @@ class SongCastLayout(
                 GlobalScope.launch(Dispatchers.Main) {
                     delay(100) // WTF: Android hackaround
                     when (response.rejoined) {
-                        true -> uiInfoService.showInfo(R.string.songcast_room_joined_rejoined, response.member_name)
-                        false -> uiInfoService.showInfo(R.string.songcast_room_joined, response.member_name)
+                        true -> uiInfoService.showInfo(
+                            R.string.songcast_room_joined_rejoined,
+                            response.member_name
+                        )
+
+                        false -> uiInfoService.showInfo(
+                            R.string.songcast_room_joined,
+                            response.member_name
+                        )
                     }
                 }
             }
@@ -131,4 +160,116 @@ class SongCastLayout(
         })
     }
 
+}
+
+class StateModel : ViewModel() {
+    var myName: String by mutableStateOf("")
+    var roomCode: String by mutableStateOf("")
+}
+
+@Composable
+private fun MainPage(layout: SongCastLayout) {
+    var tabIndex by remember { mutableStateOf(0) }
+    val tabNames = listOf("Create Room", "Join Room")
+
+    Column {
+        RichText(R.string.songcast_feature_hint)
+
+        OutlinedTextField(
+            value = layout.stateModel.myName,
+            onValueChange = { layout.stateModel.myName = it },
+            label = { Text(stringResource(R.string.songcast_hint_my_name)) },
+            singleLine = true,
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        layout.randomizeName()
+                    },
+                ) {
+                    Icon(
+                        Icons.Filled.Refresh,
+                        contentDescription = stringResource(R.string.songcast_randomize_name),
+                    )
+                }
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        Spacer(modifier = Modifier.size(16.dp))
+
+        TabRow(selectedTabIndex = tabIndex) {
+            tabNames.forEachIndexed { index, title ->
+                Tab(
+                    text = { Text(title) },
+                    selected = tabIndex == index,
+                    onClick = { tabIndex = index }
+                )
+            }
+        }
+        when (tabIndex) {
+            0 -> TabCreateRoom(layout)
+            1 -> TabJoinRoom(layout)
+        }
+    }
+}
+
+@Composable
+private fun TabCreateRoom(layout: SongCastLayout) {
+    Column {
+        Button(
+            onClick = {
+                GlobalScope.launch {
+                    safeExecute {
+                        layout.createRoom()
+                    }
+                }
+            },
+            modifier = Modifier.fillMaxWidth(),
+            elevation = ButtonDefaults.buttonElevation(defaultElevation = 3.0.dp),
+        ) {
+            Icon(
+                Icons.Filled.Add,
+                contentDescription = null,
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+                tint = md_theme_light_primaryContainer,
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text(stringResource(R.string.songcast_create_new_room))
+        }
+    }
+}
+
+@Composable
+private fun TabJoinRoom(layout: SongCastLayout) {
+    Column {
+        LabelText(R.string.songcast_enter_room_number_to_join)
+        OutlinedTextField(
+            value = layout.stateModel.roomCode,
+            onValueChange = { layout.stateModel.roomCode = it },
+            label = { stringResource(R.string.songcast_room_code_hint) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Button(
+            onClick = {
+                GlobalScope.launch {
+                    safeExecute {
+                        layout.joinRoom()
+                    }
+                }
+            },
+            modifier = Modifier.fillMaxWidth(),
+            elevation = ButtonDefaults.buttonElevation(defaultElevation = 3.0.dp),
+        ) {
+            Icon(
+                Icons.Filled.Check,
+                contentDescription = null,
+                modifier = Modifier.size(ButtonDefaults.IconSize),
+                tint = md_theme_light_primaryContainer,
+            )
+            Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+            Text(stringResource(R.string.songcast_join_room))
+        }
+    }
 }
