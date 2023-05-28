66a5b0add3fc7297c337671f598329cba5a8d5a1
igrek51 <igrek51.dev@gmail.com>
2023-05-28 21:37:12 +0200

    Send contact messages with http requester

--- app/src/main/java/igrek/songbook/send/SendMessageService.kt
+++ app/src/main/java/igrek/songbook/send/SendMessageService.kt
@@ -1,37 +1,37 @@
 package igrek.songbook.send
 
-import android.os.Handler
-import android.os.Looper
 import igrek.songbook.R
+import igrek.songbook.admin.HttpRequester
 import igrek.songbook.info.UiInfoService
-import igrek.songbook.info.logger.LoggerFactory
+import igrek.songbook.info.errorcheck.UiErrorHandler
 import igrek.songbook.inject.LazyExtractor
 import igrek.songbook.inject.LazyInject
 import igrek.songbook.inject.appFactory
 import igrek.songbook.layout.LayoutController
 import igrek.songbook.persistence.repository.SongsRepository
 import igrek.songbook.system.PackageInfoService
-import okhttp3.*
-import java.io.IOException
+import igrek.songbook.util.ioScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import okhttp3.MultipartBody
+import okhttp3.Request
 
 class SendMessageService(
     uiInfoService: LazyInject<UiInfoService> = appFactory.uiInfoService,
-    okHttpClient: LazyInject<OkHttpClient> = appFactory.okHttpClient,
     packageInfoService: LazyInject<PackageInfoService> = appFactory.packageInfoService,
     songsRepository: LazyInject<SongsRepository> = appFactory.songsRepository,
     layoutController: LazyInject<LayoutController> = appFactory.layoutController,
 ) {
     private val uiInfoService by LazyExtractor(uiInfoService)
-    private val okHttpClient by LazyExtractor(okHttpClient)
     private val packageInfoService by LazyExtractor(packageInfoService)
     private val songsRepository by LazyExtractor(songsRepository)
     private val layoutController by LazyExtractor(layoutController)
 
-    private val logger = LoggerFactory.logger
+    private val httpRequester = HttpRequester()
 
     companion object {
-        private const val APPLICATION_ID = 1
         private const val url = "https://feedback.igrek.dev/api/v1/send"
+        private const val APPLICATION_ID = 1
     }
 
     fun sendContactMessage(
@@ -43,65 +43,45 @@ class SendMessageService(
         originalSongId: String? = null,
     ) {
         uiInfoService.showInfo(R.string.contact_sending, indefinite = true)
+        ioScope.launch {
+            val appVersionName = packageInfoService.versionName
+            val appVersionCode = packageInfoService.versionCode.toString()
+            val dbVersionNumber = songsRepository.publicSongsRepo.versionNumber.toString()
 
-        val appVersionName = packageInfoService.versionName
-        val appVersionCode = packageInfoService.versionCode.toString()
-        val dbVersionNumber = songsRepository.publicSongsRepo.versionNumber.toString()
+            val requestBody = MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("message", message)
+                .addFormDataPart("subject", subject ?: "")
+                .addFormDataPart("author", author ?: "")
+                .addFormDataPart("title", title ?: "")
+                .addFormDataPart("category", category ?: "")
+                .addFormDataPart("origin_id", origin.id.toString())
+                .addFormDataPart("original_song_id", originalSongId ?: "")
+                .addFormDataPart("application_id", APPLICATION_ID.toString())
+                .addFormDataPart("app_version", "$appVersionName ($appVersionCode)")
+                .addFormDataPart("db_version", dbVersionNumber)
+                .build()
 
-        val requestBody = MultipartBody.Builder().setType(MultipartBody.FORM)
-            .addFormDataPart("message", message)
-            .addFormDataPart("subject", subject ?: "")
-            .addFormDataPart("author", author ?: "")
-            .addFormDataPart("title", title ?: "")
-            .addFormDataPart("category", category ?: "")
-            .addFormDataPart("origin_id", origin.id.toString())
-            .addFormDataPart("original_song_id", originalSongId ?: "")
-            .addFormDataPart("application_id", APPLICATION_ID.toString())
-            .addFormDataPart("app_version", "$appVersionName ($appVersionCode)")
-            .addFormDataPart("db_version", dbVersionNumber)
-            .build()
+            val request = Request.Builder().url(url).post(requestBody).build()
 
-        val request = Request.Builder().url(url).post(requestBody).build()
+            val result = httpRequester.httpRequestAsync(request) {
+                it.body()?.string() ?: ""
+            }.await()
 
-        okHttpClient.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                onErrorReceived(e.message)
-            }
-
-            @Throws(IOException::class)
-            override fun onResponse(call: Call, response: Response) {
-                if (!response.isSuccessful) {
-                    onErrorReceived("Unexpected code: $response")
-                } else {
-                    onResponseReceived(response.body()!!.string())
-                }
-            }
-        })
-    }
-
-    private fun onResponseReceived(response: String) {
-        logger.debug("Message sent response: $response")
-        Handler(Looper.getMainLooper()).postDelayed(
-            {
+            result.fold(onSuccess = { response: String ->
                 if (response.startsWith("200")) {
+                    delay(500) // if sending too fast, user is not sure if it has been sent
                     uiInfoService.showInfo(R.string.contact_message_sent_successfully)
                 } else {
-                    onErrorReceived("Contact message sent bad response: $response")
+                    val error = RuntimeException("Bad response: $response")
+                    UiErrorHandler.handleError(error, R.string.contact_error_sending)
                 }
-            },
-            500
-        ) // additional delay due to sending is "too fast" (user is not sure if it has been sent)
-    }
-
-    private fun onErrorReceived(errorMessage: String?) {
-        logger.error("Contact message sending error: $errorMessage")
-        Handler(Looper.getMainLooper()).post {
-            uiInfoService.showInfo(R.string.contact_error_sending, indefinite = true)
+            }, onFailure = { e ->
+                UiErrorHandler.handleError(e, R.string.contact_error_sending)
+            })
         }
     }
 
     fun requestMissingSong() {
         layoutController.showLayout(MissingSongLayoutController::class)
     }
-
 }
