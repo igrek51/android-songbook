d7a705d2f86f427f7caaea2f883803d131774179
igrek51 <igrek51.dev@gmail.com>
2023-08-22 22:01:01 +0200

    Fix cancelling reordering animation after scrolling

--- app/src/main/java/igrek/songbook/compose/ReorderListView.kt
+++ app/src/main/java/igrek/songbook/compose/ReorderListView.kt
@@ -302,6 +302,7 @@ private fun <T> Modifier.createReorderButtonModifier(
                     onReorder(itemsContainer.items)
                 }
                 else -> coroutineScope.launch {
+                    itemAnimatedOffsets[index]?.snapTo(relativateOffset)
                     itemAnimatedOffsets[index]?.animateTo(0f)
                 }
             }
@@ -317,7 +318,9 @@ private fun <T> Modifier.createReorderButtonModifier(
             dragTargetIndex.value = null
             scrollJob.value?.cancel()
             scrollJob.value = null
+            val relativateOffset = (itemAnimatedOffsets[index]?.targetValue ?: 0f) + scrollDiff.value
             coroutineScope.launch {
+                itemAnimatedOffsets[index]?.snapTo(relativateOffset)
                 itemAnimatedOffsets[index]?.animateTo(0f)
             }
         },
