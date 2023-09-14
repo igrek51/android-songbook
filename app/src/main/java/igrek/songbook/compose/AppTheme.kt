47b82e07d13c28e89cf85cba2317bf84473b137c
igrek51 <igrek51.dev@gmail.com>
2023-09-14 21:37:12 +0200

    Apply white ripple theme

--- app/src/main/java/igrek/songbook/compose/AppTheme.kt
+++ app/src/main/java/igrek/songbook/compose/AppTheme.kt
@@ -1,8 +1,12 @@
 package igrek.songbook.compose
 
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 
@@ -10,13 +14,30 @@ import androidx.compose.ui.text.TextStyle
 fun AppTheme(
     content: @Composable () -> Unit
 ) {
+    val textStyle = LocalTextStyle.current.merge(TextStyle(color = Color.White))
     MaterialTheme(
         colorScheme = DarkSideColors,
         content = {
-            ProvideTextStyle(
-                value = TextStyle(color = Color.White),
+            CompositionLocalProvider(
+                LocalTextStyle provides textStyle,
+                LocalRippleTheme provides WhiteRippleTheme,
                 content = content,
             )
         }
     )
 }
+
+private object WhiteRippleTheme : RippleTheme {
+    @Composable
+    override fun defaultColor() = Color.White
+
+    @Composable
+    override fun rippleAlpha() = defaultRippleAlpha
+}
+
+private val defaultRippleAlpha = RippleAlpha(
+    pressedAlpha = 0.12f,
+    focusedAlpha = 0.12f,
+    draggedAlpha = 0.16f,
+    hoveredAlpha = 0.08f,
+)
