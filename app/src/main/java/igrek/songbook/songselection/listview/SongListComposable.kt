d5212bdd11a946de27c952001a0f4db39dd27cc2
igrek51 <igrek51.dev@gmail.com>
2023-11-02 23:04:54 +0100

    Show scrollbar on song list composable

--- app/src/main/java/igrek/songbook/songselection/listview/SongListComposable.kt
+++ app/src/main/java/igrek/songbook/songselection/listview/SongListComposable.kt
@@ -26,13 +26,18 @@ import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import igrek.songbook.R
 import igrek.songbook.compose.colorTextSubtitle
 import igrek.songbook.compose.colorTextTitle
 import igrek.songbook.compose.itemBorderStroke
+import igrek.songbook.compose.md_theme_dark_onSurfaceVariant
 import igrek.songbook.info.logger.LoggerFactory.logger
 import igrek.songbook.songselection.listview.items.AbstractListItem
 import igrek.songbook.songselection.listview.items.CategoryListItem
@@ -76,8 +81,9 @@ fun SongListComposable(
 ) {
     key(itemsContainer.modifiedAll.value) {
         LazyColumn(
-            modifier = Modifier.fillMaxHeight(),
+            modifier = Modifier.fillMaxHeight().simpleVerticalScrollbar(scrollState),
             state = scrollState,
+            userScrollEnabled = true,
         ) {
             itemsContainer.items.forEachIndexed { index: Int, item: AbstractListItem ->
                 item (key = item.id() ?: index) {
@@ -220,6 +226,31 @@ fun SongItemPostButtonComposable(
                     tint = Color.White,
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun Modifier.simpleVerticalScrollbar(
+    state: LazyListState,
+    width: Dp = 4.dp
+): Modifier {
+    return drawWithContent {
+        drawContent()
+
+        val firstVisibleElementIndex = state.layoutInfo.visibleItemsInfo.firstOrNull()?.index
+
+        if (firstVisibleElementIndex != null) {
+            val elementHeight = this.size.height / state.layoutInfo.totalItemsCount
+            val scrollbarOffsetY = firstVisibleElementIndex * elementHeight
+            val scrollbarHeight = state.layoutInfo.visibleItemsInfo.size * elementHeight
+
+            drawRect(
+                color = md_theme_dark_onSurfaceVariant,
+                topLeft = Offset(this.size.width - width.toPx(), scrollbarOffsetY),
+                size = Size(width.toPx(), scrollbarHeight),
+                alpha = 0.4f,
+            )
         }
     }
 }
